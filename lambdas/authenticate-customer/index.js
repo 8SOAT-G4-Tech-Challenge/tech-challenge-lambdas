a5a15135c79ddca65e865f44462a96b5e0515e4b
Jackson Batista <jackson.batista@mblabs.com.br>
2024-12-10 18:57:28 -0300

    feat: lambda authenticate-customer

--- lambdas/authenticate-customer/index.js
+++ lambdas/authenticate-customer/index.js
@@ -1,17 +1,55 @@
-// authenticate-customer/index.js
-/* exports.handler = async (event) => {
-  try {
-    const { cpf } = JSON.parse(event.body);
-    
-    // Query DynamoDB/RDS for existing customer
-    const customer = await findCustomerByCPF(cpf);
-    
-    if (!customer) {
-      return buildResponse(404, { message: 'Customer not found' });
-    }
-    
-    return buildResponse(200, { customer });
-  } catch (error) {
-    return buildResponse(500, { error: 'Authentication failed' });
-  }
-}; */
+const { Pool } = require('pg');
+
+const getCustomerByDocument = (document) =>
+	`SELECT * FROM public."customer" WHERE cpf = '${document}'`;
+
+const pool = new Pool({
+	user: process.env.USER,
+	host: process.env.HOST,
+	database: process.env.DATABASE,
+	password: process.env.PASSWORD,
+	port: process.env.PORT,
+	ssl: {
+		rejectUnauthorized: false,
+	},
+});
+
+exports.handler = async (event) => {
+	let client;
+	try {
+		client = await pool.connect();
+
+		if (!event?.body?.document) {
+			const error = new Error('Invalid document');
+			error.statusCode = 400;
+			throw error;
+		}
+
+		const res = await client.query(getCustomerByDocument(event.body.document));
+
+		return {
+			statusCode: 200,
+			headers: {
+				'Content-Type': 'application/json',
+				'Access-Control-Allow-Origin': '*',
+			},
+			body: JSON.stringify(res.rows),
+		};
+	} catch (error) {
+		return {
+			statusCode: error.statusCode || 500,
+			headers: {
+				'Content-Type': 'application/json',
+				'Access-Control-Allow-Origin': '*',
+			},
+			body: JSON.stringify({
+				message: error.message || 'Error when trying to get customer',
+				error: error.code,
+			}),
+		};
+	} finally {
+		if (client) {
+			client.release();
+		}
+	}
+};
