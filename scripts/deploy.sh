a8c8e340f9509399aadfbbeb83b47a78f264cccd
Jackson Batista <jackson.batista@mblabs.com.br>
2024-12-14 17:50:06 -0300

    fix: update pipeline

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,7 +10,7 @@ do
   cd "$folder" || { echo "Failed to enter $folder"; exit 1; }
   folder_name=$(basename "$folder") 
 
-  if aws lambda create-function --function-name tech-challenge-$folder_name --runtime nodejs18.x --handler index.handler --role arn:aws:iam::$AWS_ACCOUNT_ID:role/LabRole --zip-file fileb://../../$folder/$folder_name.zip --environment "Variables={USER=$DB_USER,HOST='$DB_AWS_HOST',DATABASE='$DB_DATABASE',PASSWORD='$DB_PASSWORD',PORT='$DB_PORT'}" --no-cli-pager; then
+  if aws lambda create-function --function-name tech-challenge-$folder_name --runtime nodejs18.x --handler index.handler --role arn:aws:iam::$AWS_ACCOUNT_ID:role/LabRole --zip-file fileb://$folder/$folder_name.zip --environment "Variables={USER=$DB_USER,HOST='$DB_AWS_HOST',DATABASE='$DB_DATABASE',PASSWORD='$DB_PASSWORD',PORT='$DB_PORT'}" --no-cli-pager; then
     echo "Deploy completed for $folder"
   else
     echo "Deploy failed for $folder"
