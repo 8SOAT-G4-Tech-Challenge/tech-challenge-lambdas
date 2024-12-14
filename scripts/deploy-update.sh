e4eddcb19bb424734c249a59bba2d649bc3e1346
Jackson Batista <jackson.batista@mblabs.com.br>
2024-12-14 17:42:53 -0300

    fix: update pipeline

--- scripts/deploy-update.sh
+++ scripts/deploy-update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-base_folder="../lambdas"
+base_folder="lambdas"
 
 folders=$(find "$base_folder" -mindepth 1 -maxdepth 1 -type d)
 
@@ -8,12 +8,14 @@ do
   echo "Deploying $folder..."
   
   cd "$folder" || { echo "Failed to enter $folder"; exit 1; }
+  folder_name=$(basename "$folder") 
 
-  if aws lambda update-function-code --function-name tech-challenge-$folder --zip-file fileb://$folder.zip; then
-    echo "Deploy completed for $folder"
-  else
-    echo "Deploy failed for $folder"
-    exit 1;
+  if aws lambda update-function-code --function-name tech-challenge-$folder_name --zip-file fileb://../../$folder/$folder_name.zip --no-cli-pager; then
+    echo "Lambda update completed for $folder"
+  elif aws lambda create-function --function-name tech-challenge-$folder_name --runtime nodejs18.x --handler index.handler --role arn:aws:iam::$AWS_ACCOUNT_ID:role/LabRole --zip-file fileb://../../$folder/$folder_name.zip --environment "Variables={USER=$DB_USER,HOST='$DB_AWS_HOST',DATABASE='$DB_DATABASE',PASSWORD='$DB_PASSWORD',PORT='$DB_PORT'}" --no-cli-pager; then
+    echo "Lambda deployed for $folder"
+  else 
+    echo "Deployment failed for $folder"
   fi
   
   cd ../..
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-base_folder="../lambdas"
+base_folder="lambdas"
 
 folders=$(find "$base_folder" -mindepth 1 -maxdepth 1 -type d)
 
@@ -8,8 +8,9 @@ do
   echo "Deploying $folder..."
   
   cd "$folder" || { echo "Failed to enter $folder"; exit 1; }
+  folder_name=$(basename "$folder") 
 
-  if aws lambda create-function --function-name tech-challenge-$folder --runtime nodejs18.x --handler index.handler --role arn:aws:iam::$AWS_ACCOUNT_ID:role/LabRole --zip-file fileb://$folder.zip --environment "Variables={USER=$DB_USER,HOST='$DB_AWS_HOST',DATABASE='$DB_DATABASE',PASSWORD='$DB_PASSWORD',PORT='$DB_PORT'}"; then
+  if aws lambda create-function --function-name tech-challenge-$folder_name --runtime nodejs18.x --handler index.handler --role arn:aws:iam::$AWS_ACCOUNT_ID:role/LabRole --zip-file fileb://../../$folder/$folder_name.zip --environment "Variables={USER=$DB_USER,HOST='$DB_AWS_HOST',DATABASE='$DB_DATABASE',PASSWORD='$DB_PASSWORD',PORT='$DB_PORT'}" --no-cli-pager; then
     echo "Deploy completed for $folder"
   else
     echo "Deploy failed for $folder"
